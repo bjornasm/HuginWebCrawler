5a98b076c4d5ea81a667b224328116a499cfc13d
WindowsPC Bjornar <bjornasm@gmail.com>
2014-04-29 17:01:15 +0200

    Added exception of mailto. A try/catch would be a good solution

--- webcrawler.py
+++ webcrawler.py
@@ -9,7 +9,7 @@ def crawler(url):
         	new_url = re.sub('\/*$', '', new_url)
 
 		# A bit ugly, but this is to be sure that the new url is not in the urls already crawled, or scheduled for crawling, a check for if it's a HTML element that have escape the regex, or a "link" to a html class on the same page, and that the url ending is in the allowed list
-		if not any(new_url in word for word in urls_Crawled) and not any(new_url in word for word in urlsToCrawl_Parent)and not any(new_url in word for word in urlsToCrawl_Child) and '<' not in new_url and '#' not in new_url and any(word in new_url.split('.')[-1] for word in allowedList) and 'https://wow' not in new_url:
+		if not any(new_url in word for word in urls_Crawled) and not any(new_url in word for word in urlsToCrawl_Parent)and not any(new_url in word for word in urlsToCrawl_Child) and '<' not in new_url and '#' not in new_url and any(word in new_url.split('.')[-1] for word in allowedList) and 'https://wow' not in new_url and 'mailto' not in new_url:
 			
 			# If the url is in the new url, f.ex http://www.telenor.com is inside http://www.telenor.com/no/personvern/feed
 			# the latter should be appended in the parent list as i prioritize the local urls first.

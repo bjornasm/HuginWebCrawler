5598d793b1207de61af4d26e610d7b8415e09856
WindowsPC Bjornar <bjornasm@gmail.com>
2014-04-29 11:35:39 +0200

    Bedre sjekk for parent/child

--- webcrawler.py
+++ webcrawler.py
@@ -7,25 +7,25 @@ def crawler(url):
 
 	# The crawler find links by utilizing pythons urllib and the href tag
 	for new_url in re.findall('''href=["'](.[^"']+)["']''', urllib.urlopen(url).read()):
+                new_url = re.sub('\/*$', '', new_url)
 
 		# A bit ugly, but this is to be sure that the new url is not in the urls already crawled, or scheduled for crawling, a check for if it's a HTML element that have escape the regex, or a "link" to a html class on the same page, and that the url ending is in the allowed list
-		if not any(new_url in word for word in urls_Crawled) and not any(new_url in word for word in urlsToCrawl_Parent) and not any(new_url in word for word in urlsToCrawl_Child) and '<' not in new_url and '#' not in new_url and any(word in new_url.split('.')[-1] for word in allowedList):
+		if not any(new_url in word for word in urls_Crawled) and not any(new_url in word for word in urlsToCrawl_Parent)and not any(new_url in word for word in urlsToCrawl_Child) and '<' not in new_url and '#' not in new_url and any(word in new_url.split('.')[-1] for word in allowedList):
 			
 			# If the url is in the new url, f.ex http://www.telenor.com is inside http://www.telenor.com/no/personvern/feed
 			# the latter should be appended in the parent list as i prioritize the local urls first.
-			if url in new_url:
+			if url.replace("http://","").replace("www.","").split('/', 1)[0] in new_url:
 				urlsToCrawl_Parent.append(new_url)
 
 			# Another check, this is if we deal with a local link on the form /no/personvern/feed/
-			elif "http" not in new_url and "www" not in new_url:
+			elif new_url.startswith('/'):
 
 				# To better store local links, f.ex /no/personvern/feed/ forward slash (/) is omitted
-				new_url = re.sub('^\/*', '', new_url)
 				new_url = re.sub('\/*$', '', new_url)
 				url = re.sub('\/*$', '', url)
 
 				# The /no/personvern/feed/ url is stored as www.telenor.com/no/personvern/feed/
-				urlsToCrawl_Parent.append(url+"/"+new_url)
+				urlsToCrawl_Parent.append(url+new_url)
 
 			# If we are not dealing with an local URL we are dealing with a child URL 
 			else:
@@ -87,7 +87,7 @@ if __name__ == "__main__":
 	  		if urlsToCrawl_Parent[0] in urls_Crawled:
 	  			urlsToCrawl_Parent.pop(0)
 	  		else:
-	  			crawler(urlsToCrawl_Parent[0])
+                                crawler(urlsToCrawl_Parent[0])
 
 	  	# When we are done with all the URLs in the parent list, we add the first URL in the children list
 	 	urlsToCrawl_Parent.append(urlsToCrawl_Child[0])
